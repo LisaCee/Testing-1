08fb1f68a8e354a5936058737898e3cd3cb2acc5
Lisa Cee <LisaCee@users.noreply.github.com>
2018-06-29 16:58:35 -0700

    Add files via upload
    Personal copy

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,195 @@
-const helpers = require('./project-1');
+const helpers = require('./project-1.js');
 
 // start testing!
+//1
+
+describe('Project1', () => {
+    describe('multiplyByTen', () => {
+        it('Should multiply a number by ten', () => {
+            const ten = helpers.multiplyByTen(10);
+            const eleven = helpers.multiplyByTen(11);
+
+            expect(ten).toBe(100);
+            expect(eleven).not.toBe(111);
+        })
+    })
+    describe('subtractFive', () => {
+        it('Should subtract five from a number', () => {
+            const five = helpers.subtractFive(5);
+            const zero = helpers.subtractFive(0);
+
+            expect(five).toBe(0);
+            expect(zero).toBe(-5);
+        })
+    })
+    describe('sameLength', () => {
+        it('Should return if strings are same length', () => {
+            const differentLength = helpers.areSameLength('pizzas', 'honey');
+            const sameLength = helpers.areSameLength('blue', 'girl');
+
+            expect(sameLength).toBeTruthy();
+            expect(differentLength).not.toBeNaN();
+            expect(differentLength).toBeFalsy();
+        })
+    })
+    describe('areEqual', () => {
+        it('Should return true given two equal numbers', () => {
+            const equal = helpers.areEqual(12, 12);
+            const inEqual = helpers.areEqual(12, 13);
+
+            expect(equal).toEqual(true);
+            expect(equal).toBeTruthy();
+            expect(inEqual).toEqual(false)
+        })
+    })
+    describe('lessThanNinety', () => {
+        it('Should return true if a number is less than ninety', () => {
+            const ninety = helpers.lessThanNinety(90);
+            const four = helpers.lessThanNinety(4);
+            const twoHundred = helpers.lessThanNinety(200);
+    
+            expect(ninety).toBeFalsy();
+            expect(four).toBeTruthy();
+            expect(four).toBe(true);
+            expect(twoHundred).toBe(false);
+        })
+    })
+    describe('greaterThanFifty', () => {
+        it('Should return true if a number is greater than fifty', () => {
+            const fifty = helpers.greaterThanFifty(50);
+            const fiveHundred = helpers.greaterThanFifty(500);
+            const two = helpers.greaterThanFifty(2);
+
+            expect(fifty).toBeFalsy();
+            expect(fiveHundred).toBeTruthy();
+            expect(two).not.toBe(true);
+            expect(2).toBeLessThan(50);
+        })
+    })
+    describe('add', () => {
+        it('Should return the sum of two numbers', () => {
+            const four = helpers.add(2, 2);
+            const negative = helpers.add(-1, -5);
+
+            expect(four).toEqual(4);
+            expect(negative).toBe(-6);
+        })
+    })  
+    describe('subtract', () => {
+        it('Should return the difference between two numbers', () => {
+            const ten = helpers.subtract(15, 5);
+            expect(ten).toEqual(10);
+        })
+    })
+    describe('divide', () => {
+        it('Should return the quotient of two numbers', () => {
+            const ten = helpers.divide(30, 3);
+            expect(ten).toEqual(10);
+        })
+    })
+    describe('multiply', () => {
+        it('Should return the product of two numbers', () => {
+            const ten = helpers.multiply(5, 2);
+            expect(ten).toBe(10);
+        })
+    })
+    describe('getRemainder', () => {
+        it('Should return the remainder from two divided numbers', () => {
+            const one = helpers.getRemainder(11, 2);
+            expect(one).toBe(1);
+        })
+    })
+    describe('isEven', () => {
+        it('Should return if a number is even', () => {
+            const two = helpers.isEven(2);
+            const three = helpers.isEven(3);
+            expect(two).toBe(true);
+            expect(three).toBeFalsy();
+        })
+    })
+    describe('isOdd', () => {
+        it('Should return if a number is odd', () => {
+            const two = helpers.isOdd(2);
+            const three = helpers.isOdd(3);
+            expect(two).toBe(false);
+            expect(three).toBeTruthy();
+        })
+    })
+    describe('square', () => {
+        it('Should return a number squared', () => {
+            const nine = helpers.square(3);
+            expect(nine).toEqual(9);
+        })
+    })
+    describe('cube', () => {
+        it('Should return a number cubed', () => {
+            const twentySeven = helpers.cube(3);
+            expect(twentySeven).toEqual(27);
+        })
+    })
+    describe('raiseToPower', () => {
+        it('Should raise a number to the power of an exponent', () => {
+            const nine = helpers.raiseToPower(3, 2);
+            expect(nine).toEqual(9);
+        })
+    })
+    describe('roundNumber', () => {
+        it('Should round a number to the nearest integer', () => {
+            const ten = helpers.roundNumber(9.5);
+            const nine = helpers.roundNumber(9.3);
+            expect(ten).toEqual(10);
+            expect(nine).toEqual(9);
+        })
+    })
+    describe('roundUp', () => {
+        it('Should round up a number', () => {
+            const ten = helpers.roundUp(9.2);
+            const nine = helpers.roundUp(8.1);
+            expect(ten).toEqual(10);
+            expect(nine).toEqual(9);
+        })
+    })
+    describe('addExclamationPoint', () => {
+        it('Should add an exclamation point to the end of a string', () => {
+            const lola = helpers.addExclamationPoint('Lola');
+            expect(lola).toEqual('Lola!');
+        })
+    })
+    describe('combineNames', () => {
+        it('Should combine two names with a space between them', () => {
+            const lola = helpers.combineNames('Lola', 'Canini');
+            expect(lola).toBe('Lola Canini')
+        })
+    })
+    describe('getGreeting', () => {
+        it('Should add Hello and and exclamation point to a name', () => {
+            const lola = helpers.getGreeting('Lola');
+            expect(lola).toBe('Hello Lola!');
+        })
+    })
+    describe('getRectangleArea', () => {
+        it('Should return the product of two sides of a rectangle', () => {
+            const eight = helpers.getRectangleArea(4, 2);
+            expect(eight).toEqual(8);
+        })
+    })
+    describe('getTriangleArea', () => {
+        it('Should return the area of a triangle', () => {
+            const area = helpers.getRectangleArea(4, 2);
+            const mathWork = helpers.getRectangleArea(4, 2);
+            expect(area).toEqual(mathWork);
+        })
+    })
+    describe('getCircleArea', () => {
+        it('Should return the area of a circle', () => {
+            const area = helpers.getCircleArea(4);
+            const mathWork = helpers.getCircleArea(4);
+            expect(area).toEqual(mathWork);
+        })
+    })
+    describe('getRectangularPrismVolume', () => {
+        const eight = helpers.getRectangularPrismVolume(2, 2, 2);
+        expect(eight).toEqual(8);
+    })
+})
+
--- basic-javascript/project-2.spec.js
+++ basic-javascript/project-2.spec.js
@@ -1,3 +1,166 @@
 const funcs = require('./project-2');
 
 // whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+beforeEach(() => {
+    array = [1, 2, 3, 4, 5];
+})
+
+describe('Project2', () => {
+
+    describe('get biggest', () => {
+        it('Should return the greater of two numbers', () => {
+            const two = funcs.getBiggest(2, 0);
+            const thirty = funcs.getBiggest(15, 30);
+
+            expect(two).toBeGreaterThan(0);
+            expect(thirty).toBe(30)
+        })
+    })
+    describe('greeting', () => {
+        it('Should return the correct language greeting', () => {
+            const german = funcs.greeting('German');
+            const spanish = funcs.greeting('Spanish');
+            const english = funcs.greeting('English');
+
+            expect(german).toBe('Guten Tag!');
+            expect(spanish).toBe('Hola!');
+            expect(english).toBe('Hello!')
+        });
+    })
+    describe('isTenOrFive', () => {
+        it('Should return if a number is ten or five', () => {
+            const ten = funcs.isTenOrFive(10);
+            const five = funcs.isTenOrFive(5);
+            const four = funcs.isTenOrFive(4);
+
+            expect(ten).toBe(true);
+            expect(five).toBeTruthy();
+            expect(four).not.toBeTruthy();
+        })
+    })
+    describe('isInRange', () => {
+        it('Should return if a number is within range', () => {
+            const fifty = funcs.isInRange(50);
+            const thirty = funcs.isInRange(30);
+            const zero = funcs.isInRange(0);
+            expect(fifty).toBeFalsy();
+            expect(thirty).toBe(true);
+            expect(zero).toBe(false);
+        })
+    })
+    describe('isInteger', () => {
+        it('Should return if a number is an integer', () => {
+            const not = funcs.isInteger(1.1);
+            const yes = funcs.isInteger(100);
+            expect(not).toBe(false);
+            expect(yes).toBeTruthy();
+        })
+    })
+    describe('fizzBuzz', () => {
+        it('Should follow the rules of fizzBuzz', () => {
+            const fb = funcs.fizzBuzz(15);
+            const b = funcs.fizzBuzz(10);
+            const f = funcs.fizzBuzz(3);
+            const num = funcs.fizzBuzz(1);
+            expect(fb).toBe('fizzbuzz');
+            expect(b).toBe('buzz');
+            expect(f).toBe('fizz');
+            expect(num).toEqual(1);
+        })
+    })
+    describe('isPrime', () => {
+        it('Should return if a number is prime', () => {
+            const prime = funcs.isPrime(3);
+            const not = funcs.isPrime(10);
+            expect(prime).toBe(true);
+            expect(not).toBe(false);
+        })
+    })
+    describe('returnFirst', () => {
+        it('Should return the first item in an array', () => {
+            const first = funcs.returnFirst(array);
+            expect(first).toBe(1);
+            expect(first).not.toBe(6);
+            expect(first).not.toBe(undefined);
+        })
+    })
+    describe('returnLast', () => {
+        it('Should return the last item in an array', () => {
+            const last = funcs.returnLast(array);
+            expect(last).toBe(5);
+            expect(last).not.toBe(1);
+        })
+    })
+    describe('getArrayLength', () => {
+        it('Should return the length of an array', () => {
+            const leng = funcs.getArrayLength(array);
+            expect(leng).toBe(5);
+        })
+    })
+    describe('incrementByOne', () => {
+        it('Should add one to each item in an array', () => {
+            let array = [1, 2, 3, 4, 5];
+            funcs.incrementByOne(array);
+            expect(array).toEqual([2, 3, 4, 5, 6]);
+            expect(array).not.toEqual([1, 2, 3, 4, 5])
+        })
+    })
+    describe('addItemToArray', () => {
+        it('Should push an item to the end of an array', () => {
+            const six = funcs.addItemToArray(array, 6);
+            expect(six).toEqual([1, 2, 3, 4, 5, 6]);
+            expect(six).not.toEqual([1, 2, 3, 4, 5])
+        })
+    })
+    describe('addItemToFront', () => {
+        it('Should add an item to the front of an array', () => {
+            const zero = funcs.addItemToFront(array, 0);
+            expect(zero).toEqual([0, 1, 2, 3, 4, 5]);
+            expect(zero).not.toEqual([1, 2, 3, 4, 5]);
+            expect(zero).not.toEqual([1, 2, 3, 4, 5, 0]);
+        })
+    })
+    describe('wordsToSentence', () => {
+        it('Should combine words into a sentence', () => {
+            let sentence = funcs.wordsToSentence(['My', 'name', 'is', 'Lisa']);
+            expect(sentence).toBe('My name is Lisa');
+            expect(sentence).not.toBe('MynameisLisa');
+            expect(sentence).not.toBe(['My', 'name', 'is', 'Lisa']);
+        })
+    })
+    describe('contains', () => {
+        it('Should return if an item is in an array', () => {
+            const four = funcs.contains(array, 4);
+            const ten = funcs.contains(array, 10);
+            expect(four).toBe(true);
+            expect(ten).toBe(false);
+        })
+    })
+    describe('addNumbers', () => {
+        it('Should add all of the numbers in an array', () => {
+            let ten = funcs.addNumbers([1, 4, 5]);
+            let negative = funcs.addNumbers([-1, -4, 5]);
+            expect(ten).toEqual(10);
+            expect(negative).toEqual(0);
+        })
+    })
+
+    describe('averageTestScores', () => {
+        it('Should return the average of an array of numbers', () => {
+            let average = funcs.averageTestScore(array);
+            expect(average).toEqual(3);
+            expect(average).not.toBe(array);
+            expect(average).not.toBe([3]);
+        })
+    })
+    describe('largestNumber', () => {
+        it('Should return the largest number in an array', () => {
+            let largest = funcs.largestNumber(array);
+            expect(largest).toBe(5);
+            expect(largest).not.toBe(1);
+            expect(largest).not.toBe(array)
+        })
+    })
+})
+
